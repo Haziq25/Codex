00af523f8b1572ed0555649d12f80a9a26e76119
Haziq25 <128284478+Haziq25@users.noreply.github.com>
2025-03-22 11:16:24 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,13 @@
-// firebase.js (Handles real-time updates)
-const firebaseConfig = {
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
     apiKey: "AIzaSyD790PK-Gnu7xRtmATApIQ54vhPZFWTJTc",
     authDomain: "escape-room-timer-62fb0.firebaseapp.com",
     databaseURL: "https://escape-room-timer-62fb0-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -10,5 +18,7 @@ const firebaseConfig = {
     measurementId: "G-B8ZER9M2DB"
   };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
